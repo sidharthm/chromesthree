d9726a1ca413e6afcb57dcd5ecef11c08d26902f
Sidharth Menon <sidharth.menon92@gmail.com>
2016-05-03 23:29:16 -0700

    log messages to screen

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){
+  var s3;//available in higher namespace
+
   $("#try").click(function(){
     accessKey = $("#access").val()
     secretKey = $("#secret").val()
     if (accessKey && secretKey){
       s3 = new AWS.S3({accessKeyId: accessKey, secretAccessKey: secretKey});
       s3.listBuckets(function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-          else     console.log(data);           // successful response
-          });
+        if (err){
+          console.log(err, err.stack); // an error occurred
+          printMessage("span#credential-message",err,"error");
+        }
+        else{
+          printMessage("span#credential-message",'Authentication Success',"success"); // successful response
+        }
+      });
     } else {
-      console.log("need creds");
+      printMessage("span#credential-message",'Please Provide Credentials',"error");
     }
   });
+
+  printMessage = function(element,message,classToAdd){
+    msgElement = $(element);
+    if (msgElement){
+      msgElement.text(message);
+      if (classToAdd) msgElement.addClass(classToAdd);
+    }
+  }
 });
